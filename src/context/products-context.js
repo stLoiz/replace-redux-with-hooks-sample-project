53ac34260722b4eadae81d15670e36df4e74f3c8
stella-loizou <loizou.stella@gmail.com>
2020-05-12 13:00:19 +0100

    Create React Context API
    In this file we store the state of the products and every other component that is wrapped with the ProductsProvider will have access to the products state

---
+++ src/context/products-context.js
@@ -0,0 +1,46 @@
+import PropTypes from 'prop-types';
+import React, { useState } from 'react';
+
+export const ProductsContext = React.createContext({
+  products: [],
+});
+
+const ProductsProvider = ({ children }) => {
+  const [productsList, setProductsList] = useState([
+    {
+      id: 'p1',
+      title: 'Red Scarf',
+      description: 'A pretty red scarf.',
+      isFavorite: false,
+    },
+    {
+      id: 'p2',
+      title: 'Blue T-Shirt',
+      description: 'A pretty blue t-shirt.',
+      isFavorite: false,
+    },
+    {
+      id: 'p3',
+      title: 'Green Trousers',
+      description: 'A pair of lightly green trousers.',
+      isFavorite: false,
+    },
+    {
+      id: 'p4',
+      title: 'Orange Hat',
+      description: 'Street style! An orange hat.',
+      isFavorite: false,
+    },
+  ]);
+  return (
+    <ProductsContext.Provider value={{ products: productsList }}>
+      {children}
+    </ProductsContext.Provider>
+  );
+};
+
+ProductsProvider.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+export default ProductsProvider;
